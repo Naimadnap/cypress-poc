be1ffbdfc4a5dd0b734a0a7c153249779bb19353
Damian Zygmuntowicz <damian.zygmuntowicz@siili.com>
2021-05-04 11:35:40 +0200

    Scond cart test

--- cypress/integration/Checkout.test.ts
+++ cypress/integration/Checkout.test.ts
@@ -4,6 +4,7 @@ import LoginModal from '../modals/LoginModal';
 import CartPage from '../pages/CartPage';
 import HomePage from '../pages/HomePage';
 import ProductPage from '../pages/ProductPage';
+import { reporters } from 'mocha';
 
 describe('Checkout', () => {
     beforeEach(() => {
@@ -14,6 +15,14 @@ describe('Checkout', () => {
                 user: 'user1',
                 password: 'password',
             },
+        });
+        cy.request('/cart').then(response => {
+            for (const item of JSON.parse(response.body)) {
+                console.log(item);
+                if (item !== undefined) {
+                    cy.request('DELETE', `/cart/${item.itemId}`);
+                }
+            }
         });
         cy.visit('/');
     });
@@ -26,5 +35,12 @@ describe('Checkout', () => {
             'have.text',
             '× Error placing order. Payment declined: amount exceeds 100.00',
         );
+    });
+    it('Add Colourful socks to cart', () => {
+        cy.visit('/detail.html?id=3395a43e-2d88-40de-b95f-e00e1502085b');
+        ProductPage.addToCart();
+        NavBar.cartButton.click();
+        CartPage.buttonProceed.click();
+        cy.url().should('have.text', 'customer-orders');
     });
 });
--- cypress/pages/ProductPage.ts
+++ cypress/pages/ProductPage.ts
@@ -1,3 +1,6 @@
+import NavBar from '../fragments/NavBar';
+import TopBar from '../fragments/TopBar';
+
 class ProductPage {
     get buttonAddToCart() {
         return cy.get('#buttonCart');
